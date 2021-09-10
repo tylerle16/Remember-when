621b680885bb3d982d509d8a0287afcd757a6233
Mickell Gladness <mickellgladness@gmail.com>
2021-09-10 16:25:46 -0400

    fixed login bug and added logout

--- routes/users.js
+++ routes/users.js
@@ -55,41 +55,19 @@ router.post('/login', async (req, res) => {
       username: req.body.username
     }
   })
-    .then((user) => {
-      if (user) {
-        res.status(400).json({
-          error: 'username already in use'
-        })
-        return
+  .then((user)=> {
+    // check user password
+    bcrypt.compare(req.body.password, user.password)
+    .then((success)=> {
+      if (success){
+        // log in user
+        req.session.user = user;
+        res.json({ message: 'successfully logged in'})
+      }else{
+        // incorrect password
+        res.status(402).json({  error: 'incorrect password'})
       }
     })
-  const user = await db.User.findOne({
-    where: {
-      username: req.body.username
-    }
-  })
-
-  if (!user) {
-    res.status(400).json({
-      error: 'could not find user with that username'
-    })
-    return
-  }
-  // check password
-  const success = await bcrypt.compare(req.body.password, user.password)
-
-  if (!success) {
-    res.status(401).json({
-      error: 'incorrect password'
-    })
-    return
-  }
-
-  // login 
-  req.session.user = user
-  // respond with success/ error
-  res.json({
-    success: 'Successfully logged in'
   })
 })
 
