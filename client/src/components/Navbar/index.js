83b817f88f9eb0e51cb5041041f079547dda83b1
Wilky <williamwilkerson27@outlook.com>
2021-09-12 10:55:04 -0400

    styled the btns

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaBars } from 'react-icons/fa'
-import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
 
 
 const Navbar = () => {
@@ -22,6 +22,9 @@ const Navbar = () => {
                         <NavLinks to='signup'>Sign Up</NavLinks>
                     </NavItem>
                 </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+                </NavBtn>
             </NavbarContainer>
         </Nav>
     )
