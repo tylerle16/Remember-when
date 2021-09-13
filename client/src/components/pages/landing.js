715c417f7bae4da98e62868080920646790d3ac7
Kevin Leon <kevinleon01@gmail.com>
2021-09-13 14:57:20 -0500

    landing page created

---
+++ client/src/components/pages/landing.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../Navbar";
+import Footer from "../Footer"
+
+const Landing = () => {
+    
+
+    return (
+        <>
+            <Navbar />
+            <Footer />
+        </>
+    )
+}
+
+export default Landing
+
