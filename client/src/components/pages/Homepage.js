75c4dc6a7f054fc001ccc55164126400fe6f7178
Wilky <williamwilkerson27@outlook.com>
2021-09-15 14:02:55 -0400

    added a card

--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import Title from '../Content/Title'
-import ImageProgressBar from '../Content/ImageProgressBar'
-import UploadForm from '../Content/UploadForm'
+// import ImageProgressBar from '../Content/ImageProgressBar'
+// import UploadForm from '../Content/UploadForm'
+import { Container, Card } from 'react-bootstrap';
 
 
 
 function Homepage() {
+
+
+
+
+
     return (
-        
-        <div>
-            
-            
-            <Title/>
-            <UploadForm/>
-            {/* <ImageProgressBar/> */}
-        </div>
+        <Container>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>
+                        <Title/>
+                    </Card.Title>
+                </Card.Body>
+            </Card>
+
+
+
+        </Container>
     )
+
 }
 
-export default Homepage
+            // <UploadForm/>
+            // <ImageProgressBar/>
+
+export default Homepage;
