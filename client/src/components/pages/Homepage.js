834c1d5ffe81287761136ca9a99ccc6c8d754942
Wilky <williamwilkerson27@outlook.com>
2021-09-15 15:39:30 -0400

    not done

--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Title from '../Content/Title'
 // import ImageProgressBar from '../Content/ImageProgressBar'
 // import UploadForm from '../Content/UploadForm'
-import { Container, Card } from 'react-bootstrap';
+import { Container, Card, Col, Row } from 'react-bootstrap';
 
 
 
@@ -14,15 +14,34 @@ function Homepage() {
 
     return (
         <Container>
+        <Col>
+            <Row>
             <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Img variant="top" src="#" />
                 <Card.Body>
                     <Card.Title>
                         <Title/>
                     </Card.Title>
                 </Card.Body>
             </Card>
-
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="#" />
+                <Card.Body>
+                    <Card.Title>
+                        <Title/>
+                    </Card.Title>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="#" />
+                <Card.Body>
+                    <Card.Title>
+                        <Title/>
+                    </Card.Title>
+                </Card.Body>
+            </Card>
+            </Row>
+        </Col>
 
 
         </Container>
