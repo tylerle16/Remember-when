54de4940f72fdf346ee9f7c9c58c39b5efe00d05
Wilky <williamwilkerson27@outlook.com>
2021-09-22 12:42:24 -0400

    added red btn

--- client/src/components/Navbar/NavbarElements.js
+++ client/src/components/Navbar/NavbarElements.js
@@ -127,3 +127,23 @@ export const NavBtnLink = styled(LinkRouter)`
 `
 
 
+export const NavBtnLinkSignout = styled(LinkRouter)`
+    border-radius: 50px;
+    background: #fff;
+    white-space: nowrap;
+    padding: 10px 22px;
+    color: #010606;
+    font-size: 14px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+    
+
+    &:hover {
+    transition: all 0.2s ease-in-out;
+    background: red;
+    color: #fff
+    }
+`
